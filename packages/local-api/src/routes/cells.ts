bedddfb3180f648ebff2bfaac59960fc361eba36
stevopritchard <stephen.a.pritchard@gmail.com>
2022-04-22 11:46:28 +0100

    Working project ready to deploy to npm.

---
+++ packages/local-api/src/routes/cells.ts
@@ -0,0 +1,50 @@
+import express from 'express';
+import fs from 'fs/promises';
+import path from 'path';
+
+interface Cell {
+  id: string;
+  content: string;
+  type: 'text' | 'code';
+}
+
+export const createCellsRouter = (filename: string, dir: string) => {
+  const router = express.Router();
+  router.use(express.json());
+
+  const fullPath = path.join(dir, filename);
+
+  router.get('/cells', async (req, res) => {
+    try {
+      // read the file
+      const result = await fs.readFile(fullPath, { encoding: 'utf-8' });
+
+      // parse a list of cells out of it
+      // send list of cells back to browser
+      res.send(JSON.parse(result));
+    } catch (err) {
+      // if read throws an error...
+      // inspect the error, see if it says that the file doesn't exist
+      if (err.code === 'ENOENT') {
+        // add code to create a file and add default cells
+        await fs.writeFile(fullPath, '[]', 'utf-8');
+        res.send([]);
+      } else {
+        throw err;
+      }
+    }
+  });
+
+  router.post('/cells', async (req, res) => {
+    // take list of cells from the request object
+    // serialise them
+    const { cells }: { cells: Cell[] } = req.body;
+
+    // write cells into the file
+    await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
+
+    res.send({ status: 'ok' });
+  });
+
+  return router;
+};
